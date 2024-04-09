08b1432616f628f3a490f0a5ffa174e28885a35d
GrizzlLearn <dylebedev.learning@gmail.com>
2024-04-10 00:06:20 +0300

    5. Прерывание нити [#1019]: fix gitignore

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -8,7 +8,7 @@ public class Wget {
     public static void main(String[] args) {
         Thread thread = new Thread(
                 () -> {
-                    for (int i = 0; i <=100; i++) {
+                    for (int i = 0; i <= 100; i++) {
                         try {
                             Thread.sleep(1000);
                             System.out.print("\rLoading : " + i + "%");
