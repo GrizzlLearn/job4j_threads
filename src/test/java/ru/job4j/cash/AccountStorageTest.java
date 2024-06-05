a4cf35df6095f765401827284fb861cb2023e678
GrizzlLearn <dylebedev.learning@gmail.com>
2024-06-05 20:47:47 +0300

    3. Денежные переводы AccountStorage [#1104] review fix 2

--- src/test/java/ru/job4j/cash/AccountStorageTest.java
+++ src/test/java/ru/job4j/cash/AccountStorageTest.java
@@ -64,19 +64,4 @@ class AccountStorageTest {
         assertThat(firstAccount.amount()).isEqualTo(100);
         assertThat(secondAccount.amount()).isEqualTo(100);
     }
-
-    @Test
-    void whenTransferNegativeAmount() {
-        var storage = new AccountStorage();
-        storage.add(new Account(1, 100));
-        storage.add(new Account(2, 100));
-        storage.transfer(1, 2, -100);
-        var firstAccount = storage.getById(1)
-                .orElseThrow(() -> new IllegalStateException("Not found account by id = 1"));
-        var secondAccount = storage.getById(2)
-                .orElseThrow(() -> new IllegalStateException("Not found account by id = 1"));
-        assertThat(firstAccount.amount()).isEqualTo(200);
-        assertThat(secondAccount.amount()).isEqualTo(0);
-    }
-
 }
